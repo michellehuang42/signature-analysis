8dbaad440d75725cbd2fdafd4e6db0dbe6b0e56d
michellehuang42 <mh3793@nyu.edu>
2017-04-02 08:21:18 -0400

    updated

--- watson.py
+++ watson.py
@@ -7,6 +7,10 @@ from os.path import join, dirname
 from os import environ
 from watson_developer_cloud import VisualRecognitionV3
 
-visual_recognition = VisualRecognitionV3('2017-04-02', api_key='{bf265b6982c2f88c4997785cd6480ed225e7171d}')
+visual_recognition = VisualRecognitionV3('2017-04-02', api_key='bf265b6982c2f88c4997785cd6480ed225e7171d')
 
-print(json.dumps(visual_recognition.classify(images_url="https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg"), indent=2))
+#route to .zip file = /Users/Michelle/Documents/Michelle/Projects/Personal Projects/Hackathons/Signature Analysis
+
+#print(json.dumps(visual_recognition.classify(images_url="https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg"), indent=2))
+
+print(json.dumps(visual_recognition.classify(images_file="/Users/Michelle/Documents/Michelle/Projects/Personal Projects/Hackathons/Signature Analysis/test1.zip"), indent=2))
